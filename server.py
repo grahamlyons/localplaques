18af719c533f30e2500b36bae5785804335e7381
Graham Lyons <graham@grahamlyons.com>
2013-04-28 19:38:22 +0100

    Use httplib2 and add a cache

--- server.py
+++ server.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template, make_response
-from urllib2 import urlopen
+from httplib2 import Http
+
+class Cache(dict):
+
+    def set(self, key, value):
+        self.__setitem__(key, value)
+
+    def delete(self, key):
+        self.__delitem__(key)
+
+    def __nonzero__(self):
+        return True
+
 
 app = Flask(__name__)
 
+cache = Cache()
+
+client = Http(cache)
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    response =  make_response(render_template("index.html"))
+    response.headers['Cache-Control'] = 'max-age=300'
+    return response
 
 @app.route("/plaques/<lat>/<lng>")
 def plaques(lat, lng):
     url = get_url(float(lat), float(lng))
-    plaque_response = urlopen(url)
-    response = make_response(plaque_response.read())
+    plaque_response, plaque_data = client.request(url)
+    response = make_response(plaque_data)
     response.headers['Content-Type'] = 'application/json'
     return response
 
@@ -31,6 +49,7 @@ def get_url(lat, lng):
     query = "?limit=50&box=[{topleft_lat},{topleft_lng}],[{bottomright_lat},{bottomright_lng}]".format(**boundary)
     url = "{0}{1}{2}".format(base_url, path, query)
     return url
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
