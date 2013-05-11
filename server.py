16e94dc597b60f20a4875f6b085fd272d4c290de
Graham Lyons <graham@grahamlyons.com>
2013-05-11 18:56:41 +0100

    Some refactoring to get the bounds of the map on the client

--- server.py
+++ server.py
@@ -25,28 +25,22 @@ def index():
     response.headers['Cache-Control'] = 'max-age=300'
     return response
 
-@app.route("/plaques/<lat>/<lng>")
-def plaques(lat, lng):
-    url = get_url(float(lat), float(lng))
+@app.route("/plaques/<nw_lat>/<nw_lng>/<se_lat>/<se_lng>")
+def plaques(**kwargs):
+    try:
+        coords = dict(((i[0], float(i[1])) for i in kwargs.items()))
+    except ValueError:
+        abort(400)
+    url = get_url(**coords)
     plaque_response, plaque_data = client.request(url)
     response = make_response(plaque_data)
     response.headers['Content-Type'] = 'application/json'
     return response
 
-def get_boundary(lat, lng):
-    delta = 0.025
-    return {
-        "topleft_lat":lat + delta,
-        "topleft_lng":lng - delta,
-        "bottomright_lat":lat - delta,
-        "bottomright_lng":lng + delta
-    }
-
-def get_url(lat, lng):
-    boundary = get_boundary(lat, lng)
+def get_url(**kwargs):
     base_url = "http://openplaques.org"
     path = "/plaques.json"
-    query = "?limit=50&box=[{topleft_lat},{topleft_lng}],[{bottomright_lat},{bottomright_lng}]".format(**boundary)
+    query = "?limit=1000&box=[{nw_lat},{nw_lng}],[{se_lat},{se_lng}]".format(**kwargs)
     url = "{0}{1}{2}".format(base_url, path, query)
     return url
 
--- static/app.js
+++ static/app.js
@@ -1,11 +1,28 @@
+/**
+ * Init map
+ * Get location
+ * Set location on map
+ * Get bounds of map
+ * Get plaque data
+ * Set plaques on map
+ */
 
-function Map(element, engine, centre) {
+function Map(element, engine) {
     this.engine = engine;
-    this.map = this.engine.map(element).setView(centre, 13);
+    this.map = this.engine.map(element, {
+        center: [51.507222, -0.1275],
+        zoom: 16
+    });
     this.engine.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
-    var marker = this.engine.marker(centre).addTo(this.map);
+}
+
+Map.prototype.setCentre = function(location) {
+    var marker = this.engine.marker(location);
+    this.map.panTo(location);
+    marker.addTo(this.map);
+    return this;
 }
 
 Map.prototype.addPlaque = function(plaque) {
@@ -14,13 +31,16 @@ Map.prototype.addPlaque = function(plaque) {
     return this;
 }
 
+Map.prototype.getBounds = function() {
+    return this.map.getBounds();
+}
+
 function Plaque(data) {
     this.data = data['plaque'];
 }
 
 Plaque.prototype.latlng = function() {
     var latlng = [];
-    console.log(this.data);
     latlng[0] = this.data['latitude'];
     latlng[1] = this.data['longitude'];
     return latlng;
@@ -36,57 +56,67 @@ Plaque.prototype.info = function() {
     return info;
 }
 
-function Client(lat, lng) {
+function Client() {
     this.xhr = new XMLHttpRequest();
-    this.xhr.open('GET', '/plaques/' + lat + '/' + lng);
 }
 
-Client.prototype.getPlaques = function(callback, errorback, context) {
-    var self = this;
+Client.prototype.getPlaques = function(bounds, callback) {
+    var self = this,
+        se = bounds.getSouthEast(),
+        nw = bounds.getNorthWest();
+    this.xhr.open('GET', '/plaques/' + 
+        new Number(nw.lat).toPrecision(5) + '/' + 
+        new Number(nw.lng).toPrecision(5) + '/' + 
+        new Number(se.lat).toPrecision(5) + '/' + 
+        new Number(se.lng).toPrecision(5) 
+    );
     this.xhr.onreadystatechange = function(e) {
         if (self.xhr.readyState == self.xhr.DONE) {
             var json = JSON.parse(self.xhr.response);
-            if (context) {
-                callback.call(context, json);
-            } else {
-                callback(json);
-            }
+            callback(json);
         }
     }
     this.xhr.send();
     return this;
 }
 
-function App(mapElement, mapEngine) {
+function App(mapElement, mapEngine, navigator) {
     this.mapElement = mapElement;
     this.mapEngine = mapEngine;
-    
-    var self = this;
-    if (this.hasGeo(navigator)) {
-        navigator.geolocation.getCurrentPosition(function(e) {
-            var location = [e.coords.latitude, e.coords.longitude];
-            self.setup(location);
-        });
-    }
+    this.nav = navigator;
 }
 
-App.prototype.hasGeo = function(navigator) {
+App.prototype.hasGeo = function() {
     var hasGeo = false;
-    if (navigator.geolocation.getCurrentPosition) {
+    if (this.nav.geolocation.getCurrentPosition) {
         hasGeo = true;
     }
     return hasGeo;
 }
 
+App.prototype.init = function() {
+    var self = this;
+    this.map = new Map(this.mapElement, this.mapEngine);
+    if (this.hasGeo()) {
+        this.nav.geolocation.getCurrentPosition(function(e) {
+            var lat = new Number(e.coords.latitude).toPrecision(5),
+                lng = new Number(e.coords.longitude).toPrecision(5),
+                location = [lat, lng];
+            self.setup(location);
+        });
+    }
+}
+
 App.prototype.setup = function(location) {
-    var map = new Map(this.mapElement, this.mapEngine, location);
-    var client = new Client(location[0], location[1]);
-    var addPlaquesToMap = function(data) {
-        var plaques = data.map(function(p){return new Plaque(p);});
-        var addPlaque = function(plaque) {
-            map.addPlaque.call(map, plaque)
-        }
-        plaques.forEach(addPlaque);
-    };
-    client.getPlaques(addPlaquesToMap);
+    this.map.setCentre(location);
+    var client = new Client(),
+        bounds = this.map.getBounds(),
+        addPlaquesToMap = function(data) {
+            var plaques = data.map(function(p){return new Plaque(p);});
+            var addPlaque = function(plaque) {
+                map.addPlaque.call(map, plaque)
+            }
+            plaques.forEach(addPlaque);
+        };
+    client.getPlaques(bounds, addPlaquesToMap);
 }
